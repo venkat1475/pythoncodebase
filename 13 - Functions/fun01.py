22335e43e42dbb451656ef8189621decc15d3de9
venkat1475 <venkatasaikrishna91590@gmail.com>
2025-07-26 12:20:58 +0530

    hello worls

---
+++ 13 - Functions/fun01.py
@@ -0,0 +1,20 @@
+#write function to calculate the sum of numbers
+first_number = 100
+second_number = 100
+def sum_of_numbers(first_number, second_number):
+    return first_number + second_number
+print(sum_of_numbers(first_number, second_number))
+
+
+#Define a function to print the current time and task name
+#Function the following parameters:
+#  your_name: Name of the task to display to output screen
+import datetime
+def print_current_time_and_task(functions):
+    current_time = datetime.datetime.now()
+    current_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+    return print(f"Current time: {current_time}, Task: {functions}")
+
+
+git config --global user.name "venkat1475
+git config --global user.email "venkatasaikrishna91590@gmail.com"
